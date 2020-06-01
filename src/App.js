7b0e8a605c2265a29dc9aeb56f57c2773b3b05f7
Cora Chan <corachan055@gmail.com>
2020-05-31 19:32:53 -0700

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+
+// import components
+import Navbar from 'react-bootstrap/Navbar';
+import { Button, Form, Nav, FormControl } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import pages
+import LandingPage from './components/landingpage';
+import About from './components/about';
+import Projects from './components/projects';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+   <Router>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/home">Cora Chan</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />        
+      <Nav className="mr-auto">
+        <Nav.Link>
+          <Link to="/">Home</Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/about">About</Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/projects">Projects</Link>
+        </Nav.Link>
+      </Nav>
+      </Navbar>
+
+      <Switch>
+        <Route exact path="/" component={LandingPage}></Route>/>
+        <Route exact path="/about" component={About}></Route>/>
+        <Route exact path="/projects" component={Projects}></Route>/>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
