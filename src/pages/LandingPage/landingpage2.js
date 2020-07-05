11d0983c41c5950bb42ae929772f9a82841c73c6
Cora Chan <corachan055@gmail.com>
2020-07-05 14:24:37 -0700

    Wrap content on smaller screens

--- src/pages/LandingPage/landingpage2.js
+++ src/pages/LandingPage/landingpage2.js
@@ -5,6 +5,7 @@ import Navbar from '../../components/navbar/navbar';
 import Footer from '../../components/footer/footer';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { useWindowDimensions } from '../../window.js';
 import './landingpage2.css';
 
 // import pictures
@@ -13,31 +14,36 @@ import Chicago from '../../img/Chicago/Streets.JPG';
 import Logo from '../../img/Homepage/Logo.png';
 
 export default function Home() {
+
+    let {width} = useWindowDimensions();
+
     return (
         <div>
             <Navbar />
             <div>
-                <Container fluid>
+                <Container>
                     <Row className="center">
                         <div>
                             <h1 className="logo"><img src={Logo} alt="Logo" /></h1>
                             <h3 className="title">CORA CHAN</h3>
                         </div>
                     </Row>
-                    <Row>
-                        <Col>
-                            <div>
-                                <img className="img1" src={Alamo} alt="Travel" />
-                                <h2 className="imgText"><Link to="/travel">TRAVEL</Link></h2>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div>
-                                <img className="img2" src={Chicago} alt="Design" />
-                                <h2 className="imgText"><Link to="/design">DESIGN</Link></h2>
-                            </div>
-                        </Col>
-                    </Row>
+                        <div className="container">
+                            <Row>
+                                <Col sm={12} lg>
+                                    <div>
+                                        <img className="img" src={Alamo} alt="Travel" />
+                                        <h2 className="imgText"><Link to="/travel">TRAVEL</Link></h2>
+                                    </div>
+                                </Col>
+                                <Col sm={12} lg>
+                                    <div>
+                                        <img className="img" src={Chicago} alt="Design" />
+                                        <h2 className="imgText"><Link to="/design">DESIGN</Link></h2>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
                 </Container>
             </div>
             <Footer />
